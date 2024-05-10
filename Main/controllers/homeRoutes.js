89af4aed90b0b6f90ccd699424aeced68bb77a73
GW <garykachunwu@gmail.com>
2024-05-09 19:15:03 -0700

    set up basic files with mvc format, finished cart

---
+++ Main/controllers/homeRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { Items } = require('../models');
+
+
+router.get('/',  async (req, res) => {
+  try {
+    const itemData = await Items.findAll({
+      order: [['name', 'ASC']],
+    });
+    const Item = itemData.map((project) => project.get({ plain: true }));
+    console.log(Item);
+    res.render('homepage', {
+      Item
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
