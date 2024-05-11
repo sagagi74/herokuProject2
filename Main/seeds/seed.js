0ab6e5905360cfbe0356f796cbe54aff85265fb0
GW <garykachunwu@gmail.com>
2024-05-11 12:02:14 -0700

    fixed seed data and home routes

--- Main/seeds/seed.js
+++ Main/seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config/connection');
 const {Product} = require('../models');
-const {Customers} = require('../models');
-const {TransactionsDetails} = require('../models');
-const {TransactionsMains} = require('../models');
+const {Customer} = require('../models');
+const {TransactionsDetail} = require('../models');
+const {TransactionsMain} = require('../models');
 
 const productData = require('./productData.json');
 const customerData = require('./customerData.json');
@@ -16,18 +16,18 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
-  await Customers.bulkCreate(customerData, {
+  await Customer.bulkCreate(customerData, {
     individualHooks: true,
     returning: true,
   });
-  await TransactionsDetails.bulkCreate(transactiondetailData, {
-    individualHooks: true,
-    returning: true,
-  });
-  await TransactionsMains.bulkCreate(transactionmainData, {
-    individualHooks: true,
-    returning: true,
-  });
+  // await TransactionsDetail.bulkCreate(transactiondetailData, {
+  //   individualHooks: true,
+  //   returning: true,
+  // });
+  // await TransactionsMain.bulkCreate(transactionmainData, {
+  //   individualHooks: true,
+  //   returning: true,
+  // });
   process.exit(0);
 };
 
