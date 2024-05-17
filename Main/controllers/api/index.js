df3746de70dccdc275ad38dbf005c6b8d17176a9
RApostolJR <romuloapostol@gmail.com>
2024-05-17 16:24:07 -0700

    updated routes for product details page

--- Main/controllers/api/index.js
+++ Main/controllers/api/index.js
@@ -2,7 +2,9 @@
 const router = require('express').Router();
 
 const customerRoutes = require('./customer-routes');
+const productDetailRoutes = require('./product-detail-routes');
 
 router.use('/customer', customerRoutes);
+router.use('/product', productDetailRoutes);
 
 module.exports = router;
