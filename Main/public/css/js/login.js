f359a1bbd9951110d83da7443c2b672eb38db866
Felix Teng <fsmunozteng@gmail.com>
2024-05-16 19:58:49 -0700

    front end js for login button

---
+++ Main/public/css/js/login.js
@@ -0,0 +1,60 @@
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+  console.log(email, password);
+
+  if (email && password) {
+    const response = await fetch('/api/customer/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    console.log(response);
+    console.log(response.ok);
+
+    if (response.ok) {
+      document.location.replace('/');  
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const firstName = document.querySelector('#first-name-signup').value.trim();
+  const lastName = document.querySelector('#last-name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+  console.log(firstName, lastName, email, password);
+
+  if (firstName && lastName && email && password) {
+    const response = await fetch('/api/customer', {
+      method: 'POST',
+      body: JSON.stringify({ 
+        first_name: firstName, 
+        last_name: lastName, 
+        email, 
+        password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to sign up.');
+    }
+  }
+};
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
