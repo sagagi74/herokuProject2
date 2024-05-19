76e39aa3d53fe5bf525eecee2659f04c2ba3d38f
GW <garykachunwu@gmail.com>
2024-05-19 10:32:06 -0700

    fixed backend code for this feature

--- Main/controllers/api/transaction.js
+++ Main/controllers/api/transaction.js
@@ -1,26 +1,38 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection.js');
 
-router.put('/', async (req, res) => {
-    console.log(req.session.customer_id);
-    // raw sql query for updating database
-    try{ 
-        console.log("WOREULKASHJDAH@W ASDAW")
-        const sqlQuery = `
-            START TRANSACTION;
-            UPDATE TransactionsMains
-            SET ordered = 1
-            WHERE ordered = 0 AND customer_id = ${req.session.customer_id};
-            
-            UPDATE TransactionsDetails
-            JOIN TransactionsMains ON TransactionsMains.transaction_id = TransactionsDetails.transaction_id
-            SET ordered = 1
-            WHERE ordered = 0 AND TransactionsMains.customer_id = ${req.session.customer_id};
-            COMMIT;
-        `;
-        await sequelize.query(sqlQuery);
-    }
-    catch (err) {
+router.put('/complete', async (req, res) => {
+    console.log(req.session.customer_id)
+    try {
+        // Disable SQL_SAFE_UPDATES
+        await sequelize.query('SET SQL_SAFE_UPDATES = 0;');
+
+        // Start a transaction
+        await sequelize.transaction(async (t) => {
+            // Update TransactionsMains table
+            await sequelize.query(`
+                UPDATE TransactionsMains
+                SET ordered = 1
+                WHERE ordered = 0 AND customer_id = ${req.session.customer_id};
+            `, { transaction: t });
+
+            // Update TransactionsDetails table
+            await sequelize.query(`
+                UPDATE TransactionsDetails AS td
+                JOIN TransactionsMains AS tm ON tm.transaction_id = td.transaction_id
+                SET td.ordered = 1
+                WHERE td.ordered = 0 AND tm.customer_id = ${req.session.customer_id};
+            `, { transaction: t });
+        })
+
+        // Commit the transaction and enable SQL_SAFE_UPDATES
+        await sequelize.query('SET SQL_SAFE_UPDATES = 1;');
+
+        res.status(200).json({ message: 'Transaction completed successfully' });
+    } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
 module.exports = router;
+
