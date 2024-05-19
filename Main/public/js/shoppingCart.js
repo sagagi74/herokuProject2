4942500cf59ded15703d78babffbe5900252add8
Gaganjit Virdi <gaganjitvirdi@MacBook-Pro-2.local>
2024-05-19 15:20:20 -0700

    fixxed erors transactioncomplete page and did some css

--- Main/public/js/shoppingCart.js
+++ Main/public/js/shoppingCart.js
@@ -1,21 +1,29 @@
 
-document
-    .querySelector('#backToProduct')
-    .addEventListener("click", function(event) {
-        event.preventDefault();
-        document.location.replace('/products');
+$(document).ready(function($) {
+    $(".btn-dark").click(function() {
+        window.location = $(this).data("href");
     });
+});
+
+
 
 document
-    .querySelector('#transaction')
-    .addEventListener("click", async () => {
-        try {
-            await fetch('/api/transaction/complete', {
-                method: 'PUT',
-            });
-          } catch (err) {
-            console.log('Error on POST');
-        }
-        document.location.replace('/transactionComplete');
-        
+.querySelector('#backToProduct')
+.addEventListener("click", function(event) {
+event.preventDefault();
+document.location.replace('/products');
+});
+
+document
+.querySelector('#transaction')
+.addEventListener("click", async () => {
+try {
+    await fetch('/api/transaction/complete', {
+        method: 'PUT',
     });
+  } catch (err) {
+    console.log('Error on POST');
+}
+
+
+});
