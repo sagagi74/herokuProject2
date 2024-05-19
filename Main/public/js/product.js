3e9fde12c5343a60338b3830fd43329872d2ac2b
RApostolJR <romuloapostol@gmail.com>
2024-05-18 17:48:48 -0700

    updated the productData.json to add more better products

--- Main/public/js/product.js
+++ Main/public/js/product.js
@@ -1,6 +1,9 @@
 const buyProductQuantity = async (event) => {
     event.preventDefault();
 
+    // gets the quantity entered by the user
+    const productQuantity = document.querySelector('#product-quantity').value;
+
     // getting the product_id
     const pageURL = new URL(window.location.href);
     const urlArr = pageURL.pathname.split('/');
@@ -9,7 +12,6 @@ const buyProductQuantity = async (event) => {
     const total = 0; // has to be declared because part of table
     // this is getting the customer_id
     const customer_id = parseInt(document.querySelector('#customerID').textContent);  // Assuming you have the customer ID available here
-    console.log(customer_id);
     const created_date = dayjs().format('YYYY-MM-DD HH:mm:ss');
     const ordered = 0; // has to be declared because part of table
     const transaction_id = 0; // has to be declared because part of table
@@ -23,23 +25,29 @@ const buyProductQuantity = async (event) => {
         product_id,
         dordered,
     };
-    try {
-        const response = await fetch(`/api/products/${product_id}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(transactionData)
-        });
-        const result = await response.json();
-        if (result.success) {
-            console.log('Data inserted successfully:', result);
-        } else {
-            console.error('Error inserting data:', result.error);
-        }
-    } catch (err) {
-        console.error('Error inserting ');
-    }
+    if (productQuantity >= 1) {
+        for (let i=0; i < productQuantity; i++) {
+        try {
+                const response = await fetch(`/api/products/${product_id}`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(transactionData)
+                });
+                const result = await response.json();
+                if (result.success) {
+                    console.log('Data inserted successfully:', result);
+                } else {
+                    console.error('Error inserting data:', result.error);
+                }
+            } catch (err) {
+                console.error('Error inserting ');
+            }
+        };
+    } else {
+        alert("Please enter a valid quantity. \nCannot add quantity of 0.")
+    };
 };
 document
     .querySelector('.product-bought')
