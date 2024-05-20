3b08617a3f3720b45a874d00c4901e2c66476697
yong woo Yun <yongwooyun@hotmail.com>
2024-05-19 20:06:06 -0700

    chaning url

--- Main/config/connection.js
+++ Main/config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize('mysql://b1ab8rzpfpd7ts4q:fwfl7g62aw2l7544@qbhol6k6vexd5qjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/pu3lh9pq7h1yuw91');
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
